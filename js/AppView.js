cbd0049c651a338a17e496e553f96d58906133cd
Lucian Lature <lucian.lature@gmail.com>
2017-06-22 16:09:05 +0100

    Implemented a way to register view callbacks.

--- js/AppView.js
+++ js/AppView.js
@@ -1,11 +1,17 @@
-import { qs } from './utils' 
+import { qs, $on } from './utils' 
 import MovieRowTemplate from './MovieRowTemplate'
 
+const bindParams = params => cb => cb(params)
 export default class AppView {
     constructor(template) {
         this.template = template
         this.$moviesList = qs('.movie-list')
         this.$main = qs('.main')
+        this.$search = qs('.search-movie')
+
+        this._cbs = {}
+
+        this.initEvents()
     }
 
     /**
@@ -15,5 +21,24 @@ export default class AppView {
 	 */
 	showMovies(movies) {
 		this.$moviesList.innerHTML = this.template.moviesList(movies)
+
+        if (movies.length) {
+            this.$main.style.display = 'block'
+        }
 	}
+
+    initEvents() {
+        $on(this.$search, 'change', ({ target }) => {
+            const search = target.value.trim()
+            this._cbs.search.call(null, search)
+        })
+    }
+
+    onSearch(cb) {
+       this._registerCallback('search', cb)
+    }
+
+    _registerCallback(name, cb) {
+        this._cbs[name] = cb
+    }
 }
